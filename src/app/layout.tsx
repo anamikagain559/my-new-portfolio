5e0ff2f2d788323b651bf16ba01b29865d7475f8
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-05-18 15:37:03 +0600

    tailwind added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "My Portfolio",
   description: "Created with the help of Frontend Tribe",
 };
-
+// this
 export default function RootLayout({
   children,
 }: Readonly<{
