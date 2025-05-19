8aa545a20471ace46dfcf115b82f99a77b503980
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-05-19 18:58:30 +0600

    hero section start

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,12 +1,28 @@
 import memojiImage from "../assets/images/memoji-computer.png";
 import Image from "next/image";
+import ArrowDown from '@/assets/icons/arrow-down.svg';
 export const HeroSection = () => {
-  return <div>
+  return <div className="py-32">
     <div className="container">
-    <Image src={memojiImage} alt="person peeking from behind laptop"/>
-  </div>
+    <Image src={memojiImage} className="size-[100px]" alt="person peeking from behind laptop"/>
+
+
   <div>
     
   </div>
+  <div>Available for new projects</div>
+  </div>
+<h1>Building Exceptional User Experiences</h1>
+<p>I Specialize in transforming designs into functional,high-performing web applications.Let's discuss your next project.</p>
+<div>
+  <button>
+    <span>Explore My Work</span>
+    <ArrowDown/>
+  </button>
+  <button>
+    <span>👋</span>
+    <span>Let's Connect</span>
+  </button>
+</div>
   </div>;
 };
