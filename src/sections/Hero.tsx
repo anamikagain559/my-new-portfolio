0b9b6b2469217372bb896333c95b3e77de4e1e96
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-05-20 21:36:05 +0600

    hero section done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,27 +2,32 @@ import memojiImage from "../assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 export const HeroSection = () => {
-  return <div className="py-32">
-    <div className="container">
-    <Image src={memojiImage} className="size-[100px]" alt="person peeking from behind laptop"/>
-
-
-  <div>
-    
-  </div>
-  <div>Available for new projects</div>
-  </div>
-<h1>Building Exceptional User Experiences</h1>
-<p>I Specialize in transforming designs into functional,high-performing web applications.Let's discuss your next project.</p>
-<div>
-  <button>
-    <span>Explore My Work</span>
-    <ArrowDown/>
-  </button>
-  <button>
-    <span>👋</span>
-    <span>Let's Connect</span>
-  </button>
+  return <div className="py-32 md:py-48">
+        <div className="container">
+          <div className="flex flex-col items-center">
+            <Image src={memojiImage} className="size-[100px]" alt="person peeking from behind laptop"/>
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center rounded-lg gap-4">
+              <div className="bg-green-500 size-2.5 rounded-full">
+                
+              </div>
+           <div className="text-sm font-medium">Available for new projects</div>
+          </div>
+        </div>
+ 
+    </div>
+<div className="max-w-lg mx-auto">
+<h1 className="font-serif text-3xl text-center mt-8 tracking-wide md:text-5xl ">Building Exceptional User Experiences</h1>
+<p className="mt-4 text-center text-white/60">I Specialize in transforming designs into functional,high-performing web applications.Let`s discuss your next project.</p>
 </div>
-  </div>;
+  <div className="flex flex-col items-center mt-8 gap-4">
+    <button  className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+      <span className="font-semibold">Explore My Work</span>
+      <ArrowDown className="size-4"/>
+    </button>
+    <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+      <span className="">👋</span>
+      <span className="font-semibold">Let`s Connect</span>
+    </button>
+  </div>
+</div>;
 };
