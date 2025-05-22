91a08df4a096c0223f23f51aa1f20dd13c187084
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-05-22 20:26:33 +0600

    project section started

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -3,12 +3,16 @@ import { Children, PropsWithChildren } from "react"
 export const HeroOrbit = ({children,size,rotation}:PropsWithChildren<{size:number;rotation:number}>) => {
     return(
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-red-500 size-[800px] ">
-    <div className="border border-red-500 rotate-45 "style={{
-    height:`${size}px`,width:`${size}px`}} >
-    <div className="border border-red-500 inline-flex ">
-      {children}
-    </div></div>
-    </div>
+                    <div className=""style={{
+                        transform:`rotate(${rotation}deg)`,
+                        height:`${size}px`,
+                        width:`${size}px`,}} >
+
+                        <div className=" inline-flex " style={{transform:`rotate(${rotation*-1}deg)`}}>
+                        {children}
+                        </div>
+                </div>
+            </div>
 
     )
 }
