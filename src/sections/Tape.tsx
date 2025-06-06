15da86023d6ec3a996cbc602cfd4f6021d0de165
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-06-06 21:47:02 +0600

    tape section done

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -9,14 +9,18 @@ const Words = [
   "Efficient",
 ]
 export const TapeSection = () => {
-  return <div>
-  <div className="flex  gap-4">
+  return <div className="py-16">
+    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1">
+     <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+      <div className="flex flex-none gap-4 py-3">
       {Words.map((word, index) => (
-      <div key={word} className="inline-flex gap-4">
-       <span>{word}</span> 
-       <StarIcon className="size-6"/>
-      </div>
+          <div key={word} className="inline-flex gap-4 items-center">
+              <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span> 
+              <StarIcon className="size-6 text-gray-900 -rotate-12"/>
+          </div>
     ))}
   </div>
+</div>
+ </div>
   </div>;
 };
